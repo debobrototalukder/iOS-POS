a62454b97f48f6163811550703032e64d7aa87b8
Ammar Joher <ammarjoher@Ammars-iMac.local>
2018-12-16 10:34:13 -0800

    Initial Commit

---
+++ iOSPOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSPOS
+//
+//  Created by Ammar Joher on 12/16/18.
+//  Copyright © 2018 Ammar Joher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
